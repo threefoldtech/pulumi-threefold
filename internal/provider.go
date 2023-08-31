7954ca9c961deef6a27352a626a63b98fb54a1b3
Omar622 <omaramin622@gmail.com>
2023-08-31 17:19:11 +0300

    Implement provider

--- internal/provider.go
+++ internal/provider.go
@@ -1,1 +1,13 @@
 package provider
+
+import (
+	p "github.com/pulumi/pulumi-go-provider"
+	"github.com/pulumi/pulumi-go-provider/infer"
+)
+
+func RunProvider(providerName, Version string) error {
+	return p.RunProvider(providerName, Version,
+		infer.Provider(infer.Options{
+			Resources: []infer.InferredResource{},
+		}))
+}
