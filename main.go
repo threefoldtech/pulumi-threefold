a9d746118e1dbe376d8b51e7780aeaf4763d08a4
Omar622 <omaramin622@gmail.com>
2023-09-03 10:40:05 +0300

    Fix typo

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 // Version is initialized by the Go linker to contain the semver of this build.
 var Version string
-var ProviderName = "pulumi-grid-provider"
+var ProviderName = "pulumi-provider-grid"
 
 func main() {
 	if err := p.RunProvider(ProviderName, Version); err != nil {
