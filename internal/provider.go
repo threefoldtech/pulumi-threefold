a99b561b7a28f9626cc0e99747a33c4b2a3d68d0
Omar622 <omaramin622@gmail.com>
2023-09-04 18:03:54 +0300

    Add example & edit network

--- internal/provider.go
+++ internal/provider.go
@@ -1,15 +1,52 @@
 package provider
 
 import (
+	"github.com/pkg/errors"
 	p "github.com/pulumi/pulumi-go-provider"
 	"github.com/pulumi/pulumi-go-provider/infer"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/diag"
+	"github.com/threefoldtech/tfgrid-sdk-go/grid-client/deployer"
 )
 
 func RunProvider(providerName, Version string) error {
 	return p.RunProvider(providerName, Version,
 		infer.Provider(infer.Options{
 			Resources: []infer.InferredResource{
-				infer.Resource[Network, NetworkArgs, NetworkState](),
+				infer.Resource[*Network, NetworkArgs, NetworkState](),
 			},
+			Config: infer.Config[*Config](),
 		}))
+}
+
+type Config struct {
+	Mnemonic string `pulumi:"mnemonic"  provider:"secret"`
+	Network  string `pulumi:"network,optional"`
+	KeyType  string `pulumi:"type,optional"`
+
+	TFPluginClient deployer.TFPluginClient
+}
+
+// var _ = (infer.Annotated)((*Config)(nil))
+
+// func (c *Config) Annotate(a infer.Annotator) {
+// 	a.Describe(&c.Mnemonic, "The mnemonic of the user. It is very secret.")
+// 	a.Describe(&c.Network, "The network to deploy on.")
+// 	a.Describe(&c.keyType, "The network to deploy on.")
+// 	a.SetDefault(&c.Network, "", "dev")
+// 	a.SetDefault(&c.keyType, "", "sr25519")
+// }
+
+var _ = (infer.CustomConfigure)((*Config)(nil))
+
+func (c *Config) Configure(ctx p.Context) error {
+	tfPluginClient, err := deployer.NewTFPluginClient(c.Mnemonic, c.KeyType, c.Network, "", "", "", 0, false)
+	if err != nil {
+		return errors.Wrap(err, "error creating threefold plugin client")
+	}
+
+	c.TFPluginClient = tfPluginClient
+
+	ctx.Log(diag.Info, "grid provider setup")
+
+	return nil
 }
--- main.go
+++ main.go
@@ -8,10 +8,13 @@ import (
 
 // Version is initialized by the Go linker to contain the semver of this build.
 var Version string
-var ProviderName = "pulumi-provider-grid"
+var ProviderName = "grid"
 
 func main() {
-	if err := p.RunProvider(ProviderName, Version); err != nil {
+	if err := p.RunProvider(ProviderName, "v1.0.0"); err != nil {
 		log.Println(err)
 	}
 }
+// go build github.com/rawdaGastan/pulumi-provider-grid/ 
+// pulumi plugin install resource pulumi-provider-grid v1.0.0 -f pulumi-provider-grid --reinstall 
+//  (cd consumer && pulumi up)
