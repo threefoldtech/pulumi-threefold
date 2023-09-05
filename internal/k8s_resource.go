e1c215f14446b5d1a6a5947f5ff1d27a5c717937
Omar622 <omaramin622@gmail.com>
2023-09-05 13:59:34 +0300

    Add create method for k8s resource

---
+++ internal/k8s_resource.go
@@ -0,0 +1,86 @@
+package provider
+
+import (
+	p "github.com/pulumi/pulumi-go-provider"
+	"github.com/pulumi/pulumi-go-provider/infer"
+	"github.com/threefoldtech/tfgrid-sdk-go/grid-client/workloads"
+	"github.com/threefoldtech/zos/pkg/gridtypes"
+)
+
+// Kubernetes controlling struct
+type Kubernetes struct{}
+
+// KubernetesArgs is defining what arguments it accepts
+type KubernetesArgs struct {
+	Master           *workloads.K8sNode  `pulumi:"master"`
+	Workers          []workloads.K8sNode `pulumi:"workers"`
+	Token            string              `pulumi:"token"`
+	NetworkName      string              `pulumi:"network_name"`
+	SolutionType     string              `pulumi:"solution_type"`
+	SSHKey           string              `pulumi:"ssh_key"`
+	NodesIPRange     map[int32]string    `pulumi:"nodes_ip_range"`
+	NodeDeploymentID map[int32]int64     `pulumi:"node_deployment_id"`
+}
+
+type KubernetesState struct {
+	KubernetesArgs
+}
+
+func parseToK8sCluster(kubernetesArgs KubernetesArgs) (workloads.K8sCluster, error) {
+
+	// parse NodesIpRange
+	nodesIPRange := make(map[uint32]gridtypes.IPNet)
+	for k, v := range kubernetesArgs.NodesIPRange {
+		ipRange, err := gridtypes.ParseIPNet(v)
+		if err != nil {
+			return workloads.K8sCluster{}, err
+		}
+
+		nodesIPRange[uint32(k)] = ipRange
+	}
+
+	// parse NodeDeploymentID
+	nodeDeploymentId := make(map[uint32]uint64)
+	for k, v := range kubernetesArgs.NodeDeploymentID {
+		nodeDeploymentId[uint32(k)] = uint64(v)
+	}
+
+	k8sCluster := workloads.K8sCluster{
+		Master:           kubernetesArgs.Master,
+		Workers:          kubernetesArgs.Workers,
+		Token:            kubernetesArgs.Token,
+		NetworkName:      kubernetesArgs.NetworkName,
+		SolutionType:     kubernetesArgs.SolutionType,
+		SSHKey:           kubernetesArgs.SSHKey,
+		NodesIPRange:     nodesIPRange,
+		NodeDeploymentID: nodeDeploymentId,
+	}
+
+	return k8sCluster, nil
+}
+
+// Create creates Kubernetes cluster and deploy it
+func (*Kubernetes) Create(ctx p.Context, name string, input KubernetesArgs, preview bool) (string, KubernetesState, error) {
+	state := KubernetesState{KubernetesArgs: input}
+	if preview {
+		return name, state, nil
+	}
+
+	k8sCluster, err := parseToK8sCluster(input)
+	if err != nil {
+		return name, state, err
+	}
+
+	config := infer.GetConfig[Config](ctx)
+	if err := config.TFPluginClient.K8sDeployer.Deploy(ctx, k8sCluster); err != nil {
+		return name, state, err
+	}
+
+	if err := config.TfPluginClient.K8sDeployer.UpdateFromRemote(ctx, k8sCluster); err != nil {
+		return name, state, err
+	}
+
+	return name, state, nil
+}
+
+// TODO: Add update & delete & read
