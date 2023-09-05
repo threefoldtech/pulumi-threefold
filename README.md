7465419652d58371ce3152de296a874eb6f7c9ff
Rodina Mohamed <75133923+Rodina222@users.noreply.github.com>
2023-09-05 14:37:44 +0300

    made requested changes

--- README.md
+++ README.md
@@ -1,14 +1,1 @@
 # pulumi-grid
-
-go build -o pulumi-resource-grid github.com/rawdaGastan/pulumi-provider-grid
-
-## Network Resource
-cd examples/network
-mkdir $PWD/state
-pulumi login --cloud-url file://$PWD/state
-pulumi stack init test
-pulumi up --yes
-pulumi up --yes
-pulumi destroy --yes
-pulumi stack rm --yes
-pulumi logout
--- internal/provider.go
+++ internal/provider.go
@@ -21,7 +21,7 @@ func RunProvider(providerName, Version string) error {
 type Config struct {
 	Mnemonic      string `pulumi:"mnemonic"  provider:"secret"`
 	Network       string `pulumi:"network,optional"`
-	KeyType       string `pulumi:"type,optional"`
+	KeyType       string `pulumi:"key_type,optional"`
 	Substrate_URL string `pulumi:"substrate_url,optional"`
 	Relay_URL     string `pulumi:"relay_url,optional"`
 	Rmb_Timeout   string `pulumi:"rmb_timeout,optional"`
@@ -36,7 +36,7 @@ func (c *Config) Annotate(a infer.Annotator) {
 	a.Describe(&c.Network, "The network to deploy on.")
 	a.Describe(&c.KeyType, "The key type registered on substrate (ed25519 or sr25519).")
 	a.Describe(&c.Substrate_URL, "The substrate url, example: wss://tfchain.dev.grid.tf/ws")
-	a.Describe(&c.Relay_URL, "The rmb proxy url, example: wss://relay.dev.grid.tf")
+	a.Describe(&c.Relay_URL, "The relay url, example: wss://relay.dev.grid.tf")
 	a.Describe(&c.Rmb_Timeout, "The timeout duration in seconds for rmb calls")
 	a.SetDefault(&c.Network, "", "dev")
 	a.SetDefault(&c.KeyType, "", "sr25519")
