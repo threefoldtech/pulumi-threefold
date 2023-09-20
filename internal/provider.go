e7488e969556581f74e9c69ff0cd3522c39805ab
rawdaGastan <rody.rawda.rr@gmail.com>
2023-09-20 16:38:57 +0300

    add sched

--- internal/provider.go
+++ internal/provider.go
@@ -15,6 +15,7 @@ func RunProvider(providerName, Version string) error {
 	return p.RunProvider(providerName, Version,
 		infer.Provider(infer.Options{
 			Resources: []infer.InferredResource{
+				infer.Resource[*Schedular, SchedularArgs, SchedularState](),
 				infer.Resource[*Network, NetworkArgs, NetworkState](),
 				infer.Resource[*Deployment, DeploymentArgs, DeploymentState](),
 				infer.Resource[*Kubernetes, KubernetesArgs, KubernetesState](),
